3207ee20452248ee5cfb381ee4ac72e17bc3d5a9
tienhung164 <hungvt164@gmail.com>
2022-11-02 16:56:58 +0700

    add

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const calculateOdd = (kef) => {
-    const obr_kef = 1 / (1 - 1 / kef)
-    const a = (obr_kef - 1) / (kef - 1)
-    return (4 * a) ** (1 / 2) / (2 * a) + 1
-}
+const netBet = [0, 0, 0]
+const payOut = [0, 0, 0]
+const liquidity = 100000
+const margin = 0.05
 
-const calculateOddwithMargin = (odds, margin, fund) => {
-    const totalFund = fund[0] + fund[1] + fund[2]
-    const margin0 = margin * fund[0] /totalFund
-    const margin1 = margin * fund[1] / totalFund
-    const margin2 = margin * fund[2] /totalFund
+const calculateOddwithMargin = (odds, margin) => {
+    const profit0 = payOut[0] - netBet[0]
+    const profit1 = payOut[1] - netBet[1]
+    const profit2 = payOut[2] - netBet[2]
+    const totalProfit = profit0 + profit1 + profit2
+    if (totalProfit === 0) {
+        margin0 = margin / 3
+        margin1 = margin / 3
+        margin2 = margin / 3
+    } else {
+        margin0 = (margin * profit0) / totalProfit
+        margin1 = (margin * profit1) / totalProfit
+        margin2 = (margin * profit2) / totalProfit
+    }
+
     return [
         1 / (1 / odds[0] + margin0),
         1 / (1 / odds[1] + margin1),
@@ -24,151 +33,81 @@ const calcFund = (liquidity, odds) => {
     return [fund0, fund1, fund2]
 }
 
-const netBet = [0, 0, 0]
-const payout = [0, 0, 0]
-const liquidity = 100000
-const margin = 0.05
-
-let odds = [1, 30, 3]
-const fund = calcFund(liquidity, odds)
+let odds = [1.314, 8.211, 8.534]
+let fund = calcFund(liquidity, odds)
 const initFund = fund
 console.log()
 console.log('init fund:', fund)
+let fundBankX, fundBank1, fundBank2
 
-let funbankx = fund[1] + netBet[0] + netBet[2] - payout[0] - payout[2]
-let funbank1 = fund[0] + netBet[1] + netBet[2] - payout[1] - payout[2]
-let funbank2 = fund[2] + netBet[0] + netBet[1] - payout[0] - payout[1]
+const putBet = (amount, outcome) => {
+    console.log('===================================')
+    let _amount = amount[0] + amount[1] + amount[2]
+    console.log('place bet amount :', _amount, 'and outcome :', outcome)
+    fundBankX =
+        fund[1] + (netBet[0] + netBet[2] - payOut[0] - payOut[2] )/2 + amount[1]
+    fundBank1 =
+        fund[0] + (netBet[1] + netBet[2] - payOut[1] - payOut[2])/2 + amount[0]
+    fundBank2 =
+        fund[2] + (netBet[0] + netBet[1] - payOut[0] - payOut[1])/2 + amount[2]
 
-let totalFund = funbank1 + funbankx + funbank2
-newOdd2 = calculateOdd(totalFund / funbank2)
-newOdd1 = calculateOdd(totalFund / funbank1)
-newOddx = calculateOdd(totalFund / funbankx)
-odds = [newOdd1, newOddx, newOdd2]
-console.log('init odd :', odds)
-console.log(' ==> 1 + m : ', 1 / newOdd1 + 1 / newOddx + 1 / newOdd2)
+    let totalFund =   fundBankX + fundBank1 + fundBank2 
+     let _newOdd2 = totalFund / fundBank2
+    let _newOdd1 = totalFund / fundBank1
+    let _newOddx = totalFund / fundBankX
+    console.log('odd no margin :', [_newOdd1, _newOddx, _newOdd2])
+    let newodds = calculateOddwithMargin([_newOdd1, _newOddx, _newOdd2], margin)
+    console.log('odd have margin :', newodds)
+    console.log(
+        ' ==> 1 + m : ',
+        1 / newodds[0] + 1 / newodds[1] + 1 / newodds[2]
+    )
+    fund[outcome] += _amount
+    netBet[outcome] += _amount
+    payOut[outcome] += _amount * newodds[outcome]
+    console.log('fund :', fund[0], fund[1], fund[2])
+    console.log('netBet :', netBet[0], netBet[1], netBet[2])
+    console.log('payOut :', payOut[0], payOut[1], payOut[2])
+}
 
+const test = [
+    {
+        amount: [1000, 0, 0],
+        outcome: 0,
+    },
+    {
+        amount: [90000, 0, 0],
+        outcome: 0,
+    },
+    {
+        amount: [100, 0, 0],
+        outcome: 0,
+    }
+    ,
+    {
+        amount: [400, 0, 0],
+        outcome: 0,
+    }
+    ,
+    {
+        amount: [100, 0, 0],
+        outcome: 0,
+    }
+    ,
+    {
+        amount: [100, 0, 0],
+        outcome: 0,
+    }
+    
+]
 
+test.map((item) => {
+    putBet(item.amount, item.outcome)
+})
 
-console.log('===================================')
-let amount = 200
-console.log('bet outcome 2 amount :', amount)
-funbankx = fund[1] + netBet[0] + netBet[2] - payout[0] - payout[2]
-funbank1 = fund[0] + netBet[1] + netBet[2] - payout[1] - payout[2]
-funbank2 = fund[2] + netBet[0] + netBet[1] - payout[0] - payout[1]
-
-totalFund = funbank1 + funbankx + funbank2 + amount 
-newOdd2 = calculateOdd(totalFund / (funbank2 + amount))
-newOdd1 = calculateOdd(totalFund / funbank1)
-newOddx = calculateOdd(totalFund / funbankx)
-odds = calculateOddwithMargin([newOdd1, newOddx, newOdd2], margin, initFund)
-console.log('new odd :', odds)
-console.log(' ==> 1 + m : ', 1 / odds[0] + 1 / odds[1] + 1 / odds[2])
-
-fund[2] += amount
-netBet[2] += amount
-payout[2] += amount * odds[2]
-console.log('funbank 0',fund[0],netBet[0], payout[0])
-console.log('funbank 1',fund[1],netBet[1], payout[1])
-console.log('funbank 2',fund[2],netBet[2], payout[2])
-
-
-console.log('===================================')
-amount = 100
-console.log('bet outcome 2 amount :', amount)
-funbankx = fund[1] + netBet[0] + netBet[2] - payout[0] - payout[2]
-funbank1 = fund[0] + netBet[1] + netBet[2] - payout[1] - payout[2]
-funbank2 = fund[2] + netBet[0] + netBet[1] - payout[0] - payout[1]
-
-totalFund = funbank1 + funbankx + funbank2 + amount 
-newOdd2 = calculateOdd(totalFund / (funbank2 + amount))
-newOdd1 = calculateOdd(totalFund / funbank1)
-newOddx =calculateOdd( totalFund / funbankx)
-odds = calculateOddwithMargin([newOdd1, newOddx, newOdd2], margin, initFund)
-console.log('new odd :', odds)
-console.log(' ==> 1 + m : ', 1 / odds[0] + 1 / odds[1] + 1 / odds[2])
-
-fund[2] += amount
-netBet[2] += amount
-payout[2] += amount * odds[2]
-console.log('funbank 0',fund[0],netBet[0], payout[0])
-console.log('funbank 1',fund[1],netBet[1], payout[1])
-console.log('funbank 2',fund[2],netBet[2], payout[2])
-
-
-console.log('===================================')
-amount = 1000
-console.log('bet outcome 2 amount :', amount)
-funbankx = fund[1] + netBet[0] + netBet[2] - payout[0] - payout[2]
-funbank1 = fund[0] + netBet[1] + netBet[2] - payout[1] - payout[2]
-funbank2 = fund[2] + netBet[0] + netBet[1] - payout[0] - payout[1]
-
-totalFund = funbank1 + funbankx + funbank2 + amount 
-newOdd2 = calculateOdd(totalFund / (funbank2 + amount))
-newOdd1 = calculateOdd(totalFund / funbank1)
-newOddx =calculateOdd( totalFund / funbankx)
-odds = calculateOddwithMargin([newOdd1, newOddx, newOdd2], margin, initFund)
-console.log('new odd :', odds)
-console.log(' ==> 1 + m : ', 1 / odds[0] + 1 / odds[1] + 1 / odds[2])
-
-fund[2] += amount
-netBet[2] += amount
-payout[2] += amount * odds[2]
-console.log('funbank 0',fund[0],netBet[0], payout[0])
-console.log('funbank 1',fund[1],netBet[1], payout[1])
-console.log('funbank 2',fund[2],netBet[2], payout[2])
-
-console.log('===================================')
-amount = 1000
-console.log('bet outcome 1 amount :', amount)
-funbankx = fund[1] + netBet[0] + netBet[2] - payout[0] - payout[2]
-funbank1 = fund[0] + netBet[1] + netBet[2] - payout[1] - payout[2]
-funbank2 = fund[2] + netBet[0] + netBet[1] - payout[0] - payout[1]
-
-totalFund = funbank1 + funbankx + funbank2 + amount 
-newOdd2 = calculateOdd(totalFund / funbank2 )
-newOdd1 = calculateOdd(totalFund / (funbank1+amount))
-newOddx =calculateOdd( totalFund / funbankx)
-odds = calculateOddwithMargin([newOdd1, newOddx, newOdd2], margin, initFund)
-console.log('new odd :', odds)
-console.log(' ==> 1 + m : ', 1 / odds[0] + 1 / odds[1] + 1 / odds[2])
-
-fund[0] += amount
-netBet[0] += amount
-payout[0] += amount * odds[0]
-
-console.log('funbank 0',fund[0],netBet[0], payout[0])
-console.log('funbank 1',fund[1],netBet[1], payout[1])
-console.log('funbank 2',fund[2],netBet[2], payout[2])
-
-
-console.log('===================================')
-amount = 200
-console.log('bet outcome 1 amount :', amount)
-funbankx = fund[1] + netBet[0] + netBet[2] - payout[0] - payout[2]
-funbank1 = fund[0] + netBet[1] + netBet[2] - payout[1] - payout[2]
-funbank2 = fund[2] + netBet[0] + netBet[1] - payout[0] - payout[1]
-
-totalFund = funbank1 + funbankx + funbank2 + amount 
-newOdd2 = calculateOdd(totalFund / funbank2 )
-newOdd1 = calculateOdd(totalFund / (funbank1+amount))
-newOddx =calculateOdd( totalFund / funbankx)
-odds = calculateOddwithMargin([newOdd1, newOddx, newOdd2], margin, initFund)
-console.log('new odd :', odds)
-console.log(' ==> 1 + m : ', 1 / odds[0] + 1 / odds[1] + 1 / odds[2])
-
-fund[0] += amount
-netBet[0] += amount
-payout[0] += amount * odds[0]
-
-
-console.log('funbank 0',fund[0],netBet[0], payout[0])
-console.log('funbank 1',fund[1],netBet[1], payout[1])
-console.log('funbank 2',fund[2],netBet[2], payout[2])
-
-
-
-console.log('===================================')
+console.log('')
+console.log('')
+console.log('')
+console.log('-----------------------------')
 console.log('total net bet', netBet[0] + netBet[1] + netBet[2])
 console.log('liquidity', liquidity)
-console.log('fund',funbank1, funbankx, funbank2)
-
